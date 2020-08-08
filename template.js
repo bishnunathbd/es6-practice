6cf58a8ceb8ca972d7e413c3c393b282552c1926
bishnunathbd <bishnunath774@gmail.com>
2020-08-08 18:18:11 +0600

    template string & multiline string

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = 'Jhankar';
+const lastName = 'Mahbub';
+console.log(firstName + ' ' + lastName + ' is a good instructor.');
+console.log(`${firstName} ${lastName} is a good instructor.`);
+
+const multiline = 'I love you\n'
+   + 'I miss you\n'
+   + 'I need you'
+console.log(multiline);
+
+const multiline2 = `I love you
+I miss you
+I need you`;
+console.log(multiline2);
